7ce989534085c8f1519d22f001fcd9aee77e93f8
victoria sabbatino wermelinger <139395443+victoriawermelinger@users.noreply.github.com>
2024-01-25 19:46:55 -0300

    CREATE MODULO 09
    variáveis

---
+++ SQL Server Developer do ZERO ao DW - MODULO 9 - ITALO MESQUITA.sql
@@ -0,0 +1,99 @@
+-- O QUE SÃO VARIAVEIS
+/*as variáveis são objetos que podem armazenar valores temporários. Elas são frequentemente utilizadas para simplificar consultas, armazenar resultados intermediários ou parâmetros, e são bastante úteis em procedimentos armazenados, funções ou blocos anônimos de código.
+
+Aqui está uma breve explicação sobre variáveis em SQL:
+
+Declaração de Variáveis:
+
+As variáveis são declaradas usando a palavra-chave DECLARE. Por exemplo:
+sql
+Copy code
+DECLARE @nomeVariavel TIPO_DE_DADO;
+Atribuição de Valores:
+
+Os valores podem ser atribuídos às variáveis usando a palavra-chave SET ou diretamente na declaração. Por exemplo:
+sql
+Copy code
+DECLARE @idade INT;
+SET @idade = 25;
+Tipos de Dados:
+
+As variáveis podem ter vários tipos de dados, como INT, VARCHAR, DATETIME, etc. O tipo de dado deve ser especificado ao declarar a variável.
+sql
+Copy code
+DECLARE @nome VARCHAR(50);
+SET @nome = 'John';
+Uso em Consultas:
+
+Variáveis podem ser utilizadas em consultas para armazenar resultados ou parâmetros. Por exemplo:
+sql
+Copy code
+DECLARE @totalVendas DECIMAL(10,2);
+SET @totalVendas = (SELECT SUM(Valor) FROM Vendas WHERE Ano = 2023);
+Escopo:
+
+O escopo de uma variável é o bloco, procedimento armazenado ou função no qual ela foi declarada. Variáveis locais têm escopo limitado ao bloco em que são definidas.
+Exemplo em um Procedimento Armazenado:
+
+Aqui está um exemplo básico de um procedimento armazenado usando variáveis:
+sql
+Copy code
+CREATE PROCEDURE ExemploProcedimento
+AS
+BEGIN
+    DECLARE @nomeCliente VARCHAR(50);
+    SET @nomeCliente = 'João';
+    
+    SELECT * FROM Clientes WHERE Nome = @nomeCliente;
+END;
+As variáveis no SQL ajudam a tornar os scripts mais dinâmicos e flexíveis, permitindo o armazenamento temporário de valores que podem ser usados em várias partes de um bloco de código ou procedimento.
+*/
+-- ULTILIZANDO VARIAVEIS NO SQL 
+
+DECLARE @MOVIMENTO DATE 
+
+SET @MOVIMENTO = '01/01/2021'
+
+SELECT @MOVIMENTO
+
+SET @MOVIMENTO = CAST(GETDATE()AS DATE)
+
+SELECT @MOVIMENTO
+------------------------------------------------
+DECLARE @MOVIMENTO_INI   DATE 
+, @MOVIMENTO_FIM		 DATE 
+, @PERCENTUAL_ACRECIMO   NUMERIC 
+
+SET @MOVIMENTO_INI = '01/01/2019'
+SET @MOVIMENTO_FIM = '31/12/2020'
+SET @PERCENTUAL_ACRECIMO = 15.00
+
+SELECT @MOVIMENTO_INI AS MOVIMENTO_INI
+, @MOVIMENTO_FIM AS MOVIMENTO_FIM
+, TITULO_RECEBER
+, TITULO
+, ENTIDADE
+, VALOR
+, @PERCENTUAL_ACRECIMO AS PERCENTUAL_JUROS
+, VALOR * (1 + ( @PERCENTUAL_ACRECIMO/100)) AS VALOR_COM_JUROS 
+FROM TITULOS_RECEBER A 
+WHERE A.MOVIMENTO >= @MOVIMENTO_INI
+ AND A.MOVIMENTO <= @MOVIMENTO_FIM
+
+ -- OUTRA FORMA 
+
+DECLARE @MOVIMENTO_INI   DATE = '01/01/2019'
+, @MOVIMENTO_FIM		 DATE = '31/12/2020'
+, @PERCENTUAL_ACRECIMO   NUMERIC (15,2) = 15.00
+
+SELECT @MOVIMENTO_INI AS MOVIMENTO_INI
+, @MOVIMENTO_FIM AS MOVIMENTO_FIM
+, TITULO_RECEBER
+, TITULO
+, ENTIDADE
+, VALOR
+, @PERCENTUAL_ACRECIMO AS PERCENTUAL_JUROS
+, VALOR * (1 + ( @PERCENTUAL_ACRECIMO/100)) AS VALOR_COM_JUROS 
+FROM TITULOS_RECEBER A 
+WHERE A.MOVIMENTO >= @MOVIMENTO_INI
+ AND A.MOVIMENTO <= @MOVIMENTO_FIM
