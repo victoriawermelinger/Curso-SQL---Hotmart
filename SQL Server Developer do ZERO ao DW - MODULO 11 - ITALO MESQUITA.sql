33f39f599e1526aa116979d79ca0dd5735cda1ad
victoria sabbatino wermelinger <139395443+victoriawermelinger@users.noreply.github.com>
2024-02-28 16:47:52 -0300

    update

--- SQL Server Developer do ZERO ao DW - MODULO 11 - ITALO MESQUITA.sql
+++ SQL Server Developer do ZERO ao DW - MODULO 11 - ITALO MESQUITA.sql
@@ -8,3 +8,21 @@ declare @V2 varchar(10) = 'Maria'
 select isnull (@v1, @v2)
 
 select case when @V1 is null then @V2 else @V1 end 
+
+create table tblnomes (
+  Nome  varchar(50)
+, Apelido varchar (30)
+
+
+select * from tblnomes
+/*
+insert into tblnomes (Nome, Apelido)
+values  ('Victoria', ' Vicky')
+,		('Matheus', 'Teteus')
+,		('Ricardo', ' ')
+,		('Samara', 'Samy')
+
+update tblnomes
+set Apelido = null
+where nome = 'Samara'
+*/
